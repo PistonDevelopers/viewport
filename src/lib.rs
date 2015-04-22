a7d058dc04f089a657d8178ca4fba6ab50ff26ac
Sven Nilsen <bvssvni@gmail.com>
2015-04-22 16:45:42 +0200

    Added `abs_transform`
    Closes https://github.com/PistonDevelopers/viewport/issues/3

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 #[derive(Copy, Clone)]
 pub struct Viewport {
     /// Viewport in pixels.
+    /// ```[x, y, width height]``` where ```(x, y)``` is lower left corner.
     pub rect: [i32; 4],
     /// The size of frame buffer in pixels.
     pub draw_size: [u32; 2],
@@ -13,3 +14,68 @@ pub struct Viewport {
     pub window_size: [u32; 2],
 }
 
+impl Viewport {
+    /// Computes absolute transform for 2D graphics,
+    /// which uses a row major 2x3 matrix.
+    ////
+    /// The origin is in the upper left corner of the viewport rectangle.
+    /// The x axis points to the right, and the y axis points down.
+    /// The units are in points (window coordinates).
+    ///
+    /// It is assumed that the underlying coordinate system is normalized
+    /// with the origin in the center, such that ```(-1.0, 1.0)``` in the
+    /// underlying coordinate system corresponds to the
+    /// upper left corner of the viewport.
+    pub fn abs_transform(&self) -> [[f64; 3]; 2] {
+        let (dw, dh) = (self.draw_size[0] as f64, self.draw_size[1] as f64);
+        let (ww, wh) = (self.window_size[0] as f64, self.window_size[1] as f64);
+        let sx = 2.0 * (dw / ww) / self.rect[2] as f64;
+        let sy = -2.0 * (dh / wh) / self.rect[3] as f64;
+        [
+            [sx, 0.0, -1.0],
+            [0.0, sy, 1.0]
+        ]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn abs() {
+        let transform = |mat: [[f64; 3]; 2], (px, py)| -> (f64, f64) {
+            (mat[0][0] * px + mat[0][1] * py + mat[0][2],
+            mat[1][0] * px + mat[1][1] * py + mat[1][2])
+         };
+
+        let viewport = Viewport {
+            rect: [0, 0, 100, 200],
+            draw_size: [100, 200],
+            window_size: [100, 200],
+        };
+        let abs = viewport.abs_transform();
+        assert_eq!(abs, [
+                [0.02, 0.0, -1.0],
+                [0.0, -0.01, 1.0]
+            ]);
+        assert_eq!(transform(abs, (0.0, 0.0)), (-1.0, 1.0));
+        assert_eq!(transform(abs, (50.0, 100.0)), (0.0, 0.0));
+        assert_eq!(transform(abs, (100.0, 200.0)), (1.0, -1.0));
+
+        let viewport = Viewport {
+            rect: [10, 10, 80, 80],
+            draw_size: [100, 100],
+            window_size: [50, 50],
+        };
+        let abs = viewport.abs_transform();
+        assert_eq!(abs, [
+                [0.05, 0.0, -1.0],
+                [0.0, -0.05, 1.0]
+            ]);
+        assert_eq!(transform(abs, (0.0, 0.0)), (-1.0, 1.0));
+        // The viewport size is 80 pixels, 40 points, therefore center is 20.
+        assert_eq!(transform(abs, (20.0, 20.0)), (0.0, 0.0));
+        assert_eq!(transform(abs, (40.0, 40.0)), (1.0, -1.0));
+    }
+}
